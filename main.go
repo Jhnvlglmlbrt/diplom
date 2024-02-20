fc7255623b37f85c8c61320c3b6e290c854672fd
Jhnvlglmlbrt <user.mail0encrypt@gmail.com>
2024-02-21 00:26:53 +0300

    initial project setup: fiber + django template

--- main.go
+++ main.go
@@ -1,22 +1,43 @@
 package main
 
 import (
-	"crypto/tls"
-	"fmt"
 	"log"
+	"os"
+
+	"github.com/Jhnvlglmlbrt/monitoring-certs/handlers"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/favicon"
+	"github.com/gofiber/template/django/v3"
+	"github.com/joho/godotenv"
 )
 
-func checkSSL(domain string) {
-	config := &tls.Config{}
-	conn, err := tls.Dial("tcp", fmt.Sprintf("%s:443", config))
+func main() {
+	app, err := initApp()
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer conn.Close()
 
-	state := conn.ConnectionState()
+	app.Static("/static", "./static")
+	app.Use(favicon.New(favicon.ConfigDefault))
+	app.Use(handlers.WithAuthenticatedUser)
+	app.Get("/", handlers.HandleGetHome)
+
+	log.Fatal(app.Listen(os.Getenv("HTTP_LISTEN_ADDR")))
 }
 
-func main() {
+func initApp() (*fiber.App, error) {
+	if err := godotenv.Load(); err != nil {
+		return nil, err
+	}
 
+	engine := django.New("./views", ".html")
+	engine.Reload(true)
+	app := fiber.New(
+		fiber.Config{
+			Views:                 engine,
+			PassLocalsToViews:     true,
+			DisableStartupMessage: true,
+		})
+
+	return app, nil
 }

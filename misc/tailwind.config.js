5a9ffe42dade21e841c3fd147775dfb5af3a9e73
Jhnvlglmlbrt <user.mail0encrypt@gmail.com>
2024-02-21 00:16:17 +0300

    initial project setup: tailwindcss

---
+++ misc/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [],
+  darkMode: false,
+  content: [],
+  theme: {
+    extend: {},
+  },
+  variants: [],
+  plugins: [],
+}
+

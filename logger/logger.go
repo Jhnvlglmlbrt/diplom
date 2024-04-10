2176143dac157b434eaa9274ba8d5724869566ea
Jhnvlglmlbrt <user.mail0encrypt@gmail.com>
2024-04-10 13:40:23 +0300

    fixed problem with logger didnt write into file

--- logger/logger.go
+++ logger/logger.go
@@ -1,6 +1,8 @@
 package logger
 
 import (
+	"io"
+	"log"
 	"os"
 
 	kitlog "github.com/go-kit/log"
@@ -9,18 +11,28 @@ import (
 var logger kitlog.Logger
 
 func Init() {
-	logout := os.Stderr
-	if logpath := os.Getenv("SENDIT_LOG"); logpath != "" {
-		if file, err := os.OpenFile(logpath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666); err == nil {
-			logout = file
-		} else {
-			panic(err)
+	var logout io.Writer
+	logpath := os.Getenv("SENDIT_LOG")
+	if logpath == "" {
+		log.Fatal("SENDIT_LOG environment variable is not set")
+	}
+	if _, err := os.Stat(logpath); err != nil {
+		_, err := os.Create(logpath)
+		if err != nil {
+			log.Fatal(err)
 		}
 	}
+	logfile, err := os.OpenFile(logpath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	logout = io.MultiWriter(logfile, os.Stderr)
+
 	logger = kitlog.NewLogfmtLogger(kitlog.NewSyncWriter(logout))
 	logger = kitlog.With(logger, "ts", kitlog.DefaultTimestampUTC, "caller", kitlog.Caller(4))
 }
 
-func Log(args ...interface{}) error {
+func Log(args ...any) error {
 	return logger.Log(args...)
 }

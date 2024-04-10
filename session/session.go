ff79d108c4e74328e98b935e00418c0657e6d507
Jhnvlglmlbrt <user.mail0encrypt@gmail.com>
2024-04-10 20:18:01 +0300

    session for subscriptions

---
+++ session/session.go
@@ -0,0 +1,14 @@
+package session
+
+import (
+	"github.com/gofiber/fiber/v2/middleware/session"
+)
+
+// InitSessionMiddleware инициализирует middleware для работы с сессиями.
+func InitSessionMiddleware() *session.Store {
+	return session.New()
+}
+
+func InitSessionMiddleware1() *session.Store {
+	return session.New()
+}

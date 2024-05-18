9e990899d11a844d30a6099bf478d024c141e63b
Jhnvlglmlbrt <user.mail0encrypt@gmail.com>
2024-05-19 01:58:56 +0300

    new routes

--- main.go
+++ main.go
@@ -45,6 +45,10 @@ func main() {
 	app.Get("/", handlers.HandleGetHome)
 
 	app.Get("/pricing", handlers.HandleGetPlans)
+	app.Get("/about", handlers.HandleGetAbout)
+
+	app.Get("/check", handlers.HandleGetCheck)
+	app.Post("/check_domain", handlers.HandleCheckDomainStatus)
 
 	app.Get("/signup", handlers.HandleGetSignup)
 	app.Post("/signup", handlers.HandleSignupWithEmail)
@@ -61,32 +65,55 @@ func main() {
 	app.Get("/update_pass", handlers.HandleGetUpdatePassword)
 	app.Post("/update_pass", handlers.HandleUpdatePassword)
 
+	// Вкладка с аккаунтом
 	account := app.Group("/account", handlers.WithMustBeAuthenticated)
 	account.Get("/", handlers.HandleAccountShow)
 	account.Post("/", handlers.HandleAccountUpdate)
 
+	// Вкладка с избранными доменами
 	favorites := app.Group("/favorites", handlers.WithMustBeAuthenticated)
 	favorites.Get("/", handlers.HandleFavoritesList)
 	favorites.Post("/add_favorite", handlers.HandleAddFavorite)
 	favorites.Post("/delete_favorite", handlers.HandleRemoveFavorite)
 
+	// Вкладка с добавлением доменов, с просмотром доменов
 	domains := app.Group("/domains", handlers.WithMustBeAuthenticated)
 	domains.Get("/", handlers.HandleDomainList)
 	domains.Post("/", handlers.HandleDomainCreate)
+	domains.Post("/delete", handlers.HandleDomainsDelete)
 	domains.Get("/new", handlers.HandleDomainNew)
 	domains.Get("/:id", handlers.HandleDomainShow)
 	domains.Get("/:id/raw", handlers.HandleDomainShowRaw)
 	domains.Post("/:id/delete", handlers.HandleDomainDelete)
 	domains.Get("/:id/test_notification", handlers.HandleSendTestNotification)
-	// app.Post("/domains", handlers.WithMustBeAuthenticated, handlers.HandlePostDomain)
-	// app.Post("/domains/new", handlers.WithMustBeAuthenticated, handlers.HandleGetDomainNew)
 
-	// app.Get("/admin", handlers.HandleGetAdmin)
+	// Admin panel (users, accounts, domains, plans)
+	app.Get("/admin/auth/callback/:accessToken", handlers.HandleAuthCallbackWithAdmin)
+	admin := app.Group("/admin", handlers.WithMustBeAuthenticated)
+	admin.Get("/users", handlers.HandleUsersList)
+	admin.Post("/users/delete", handlers.HandleUsersDelete)
+	admin.Post("/users/create", handlers.HandleUsersCreate)
+
+	admin.Get("/accounts", handlers.HandleAccountsList)
+	admin.Post("/accounts/delete", handlers.HandleAccountsDelete)
+	admin.Post("/accounts/update", handlers.HandleAccountsUpdate)
+
+	admin.Get("/domains", handlers.HandleAdminDomainList)
+	admin.Post("/domains", handlers.HandleDomainCreate)
+	admin.Post("/domains/delete", handlers.HandleAdminDomainsDelete)
+	admin.Get("/domains/:id", handlers.HandleAdminDomainShow)
+	admin.Get("/domains/:id/raw", handlers.HandleAdminDomainShowRaw)
+	admin.Post("/domains/:id/delete", handlers.HandleAdminDomainsDelete)
+	admin.Get("/domains/:id/test_notification", handlers.HandleSendTestNotification)
+
+	admin.Get("/plans", handlers.HandlePlansList)
+	admin.Post("/plans/create", handlers.HandlePlansCreate)
+	admin.Post("/plans/delete", handlers.HandlePlansDelete)
+	admin.Post("/plans/update", handlers.HandlePlansUpdate)
 
 	// app.Use(handlers.NotFoundMiddleware)
 
 	log.Fatal(app.Listen(":3000"))
-
 }
 
 func initApp() (*fiber.App, error) {
